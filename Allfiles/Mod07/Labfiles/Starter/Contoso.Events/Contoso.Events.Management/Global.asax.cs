7656cf9debb776cdf685cc094fea4a86646426df
John Adams <john@opsgility.com>
2016-06-24 03:47:31 -0500

    Fix Issue #43: Fix Lab #7 data generation errors

--- Allfiles/Mod07/Labfiles/Starter/Contoso.Events/Contoso.Events.Management/Global.asax.cs
+++ Allfiles/Mod07/Labfiles/Starter/Contoso.Events/Contoso.Events.Management/Global.asax.cs
@@ -18,6 +18,8 @@ namespace Contoso.Events.Management
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            SqlServerTypes.Utilities.LoadNativeAssemblies(Server.MapPath("~/bin"));
         }
     }
 }
--- Allfiles/Mod07/Labfiles/Starter/Contoso.Events/Contoso.Events.Web/Global.asax.cs
+++ Allfiles/Mod07/Labfiles/Starter/Contoso.Events/Contoso.Events.Web/Global.asax.cs
@@ -16,6 +16,8 @@ namespace Contoso.Events.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            SqlServerTypes.Utilities.LoadNativeAssemblies(Server.MapPath("~/bin"));
         }
     }
 }
